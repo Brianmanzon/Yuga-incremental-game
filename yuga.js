73c977db1819f989f91dfdad20a5deb75fc1afbd
Brianmanzon <81338636+Brianmanzon@users.noreply.github.com>
2022-01-31 20:39:14 -0300

    Autosave
    Autosave implemented, saving  a JSON of the gamedata into the local storage

--- yuga.js
+++ yuga.js
@@ -4,12 +4,12 @@ var gameData = {
     woodPerClickCost: 10
   }
 
-  function gatherWood() {
+function gatherWood() {
     gameData.wood += gameData.woodPerClick
     document.getElementById("woodGathered").innerHTML = gameData.wood+ " Wood gathered"
   }
 
-  function buyWoodPerClick() {
+function buyWoodPerClick() {
     if (gameData.wood >= gameData.woodPerClickCost) {
       gameData.wood -= gameData.woodPerClickCost
       gameData.woodPerClick += 1
@@ -18,6 +18,14 @@ var gameData = {
       document.getElementById("perClickUpgrade").innerHTML = "Upgrade Axe (Currently Level " + gameData.woodPerClick + ") Cost: " + gameData.woodPerClickCost + " Wood"
     }
   }
-  var mainGameLoop = window.setInterval(function() {
+var mainGameLoop = window.setInterval(function() {
     gatherWood()
   }, 1000)
+
+var savegame = JSON.parse(localStorage.getItem("yugaSave"))
+if (savegame !== null) {
+  gameData = savegame
+}
+var saveGameLoop = window.setInterval(function() {
+    localStorage.setItem("yugaSave", JSON.stringify(gameData))
+  }, 15000)
